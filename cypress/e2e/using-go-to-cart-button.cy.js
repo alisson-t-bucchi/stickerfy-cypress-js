8ed95b1acfd177ecfaf5d224c76947d012f7c054
alisson-t-bucchi <alisson.bucchi@outlook.com>
2024-12-23 21:15:35 +0000

    creating tests to buy Happy, Sad and Angry stickers

--- cypress/e2e/using-go-to-cart-button.cy.js
+++ cypress/e2e/using-go-to-cart-button.cy.js
@@ -1,18 +1,85 @@
-import { slowCypressDown } from "cypress-slow-down";
+
+
 
 describe("using go to cart button to buy stickers", () => {
-  var TIME = 3000;
-  it("open main page", () => {
+
+
+  it(" open main page, select and buy Happy Stickerfy", () => {
     cy.visit("https://stickerfy.herokuapp.com/");
-
-    cy.time(TIME);
-
+    
     cy.title().should("be.equal", "Stickerfy");
+    
+    cy.get(
+      'a[href="/add-to-cart/5dd8e2b26c26d0000a675cf9"]')
+      .contains('Add to cart')
+      .click();
+
+    cy.get(
+      'a[href="/shopping-cart/"]')
+      .contains("Shopping Cart")
+      .click();
+
+    cy.get(
+      'a[href="/checkout"]')
+      .contains("Checkout")
+      .click();
+    
+    cy.contains('h1','Checkout').should('be.visible')
+    cy.contains('h4', 'Total: $5.5').should('be.visible')
+    cy.contains('h4', 'Thanks for your order!').should('be.visible')
   });
 
-  it("select and buy Happy Stickerfy", () => {
+  it(" open main page, select and buy Sad Stickerfy", () => {
+    cy.visit("https://stickerfy.herokuapp.com/");
+    
+    cy.title().should("be.equal", "Stickerfy");
+    
     cy.get(
-      '//a[@href="/add-to-cart/5dd8e2b26c26d0000a675cf9" and text()="Add to cart"'
-    );
+      'a[href="/add-to-cart/5dd8e2b26c26d0000a675cfb"]')
+      .contains('Add to cart')
+      .click();
+
+    cy.get(
+      'a[href="/shopping-cart/"]')
+      .contains("Shopping Cart")
+      .click();
+
+    cy.get(
+      'a[href="/checkout"]')
+      .contains("Checkout")
+      .click();
+    
+    cy.contains('h1','Checkout').should('be.visible')
+    cy.contains('h4', 'Total: $7').should('be.visible')
+    cy.contains('h4', 'Thanks for your order!').should('be.visible')
   });
+
+  it(" open main page, select and buy Angry Stickerfy", () => {
+    cy.visit("https://stickerfy.herokuapp.com/");
+    
+    cy.title().should("be.equal", "Stickerfy");
+    
+    cy.get(
+      'a[href="/add-to-cart/5dd8e2b26c26d0000a675cfa"]')
+      .contains('Add to cart')
+      .click();
+
+    cy.get(
+      'a[href="/shopping-cart/"]')
+      .contains("Shopping Cart")
+      .click();
+
+    cy.get(
+      'a[href="/checkout"]')
+      .contains("Checkout")
+      .click();
+    
+    cy.contains('h1','Checkout').should('be.visible')
+    cy.contains('h4', 'Total: $4.5').should('be.visible')
+    cy.contains('h4', 'Thanks for your order!').should('be.visible')
+  });
+
+
+
+
 });
